5d93fc459b2fe567d57ccc0d60528cca74d9addb
Salem <salem.b.mabrouk@gmail.com>
2016-10-12 15:02:05 +0200

    Ajout de Tests dans Créature

--- src/test/java/edu/insightr/spellmonger/CreatureTest.java
+++ src/test/java/edu/insightr/spellmonger/CreatureTest.java
@@ -6,20 +6,20 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
-
 public class CreatureTest {
     @Test
     public void getPlayerCreatures() throws Exception {
         Player player1 = new Player("p1");
         player1.setNumberOfCreaOnBoard(4);
         Assert.assertEquals(player1.getNumberOfCreaOnBoard(), 4);
-
     }
 
     @Test
     public void attackOpponent() throws Exception {
-
+        Player player = new Player("Player");
+        player.setHp(20);
+        player.setHp(player.getHp() - 5);
+        Assert.assertEquals(player.getHp(), 15);
     }
 
     @Test
